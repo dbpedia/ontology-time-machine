75cf056b1786128933eed94506630eab82397f7b
Jenifer Tabita Ciuciu-Kiss <jenifer.tabita.ciuciu.kiss@gmail.com>
2024-05-31 12:46:41 +0200

    add testcases for static text as a return

---
+++ tests/proxy_tests.py
@@ -0,0 +1,26 @@
+import pytest
+import requests
+from ontologytimemachine.proxy import start_proxy, PORT
+
+PROXY_URL = f'http://localhost:{PORT}'
+
+@pytest.fixture(scope="module", autouse=True)
+def start_proxy_server():
+    start_proxy()
+    import time
+    time.sleep(1)
+
+def test_google_com():
+    response = requests.get(PROXY_URL + '/google.com')
+    assert response.status_code == 200
+    print(response.text)
+    assert response.text == 'This is a static response for google.com'
+
+def test_other_site():
+    response = requests.get(PROXY_URL + '/example.com')
+    assert response.status_code == 404
+    print(response.text)
+    assert response.text == 'Resource not found'
+
+if __name__ == '__main__':
+    pytest.main(["-v", __file__])
