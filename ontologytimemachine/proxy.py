35ae93b06639e6b6795720b10e032fcb20b2d79b
Jenifer Tabita Ciuciu-Kiss <jenifer.tabita.ciuciu.kiss@gmail.com>
2024-06-04 03:14:07 +0200

    Add failover mode to the proxy

--- ontologytimemachine/proxy.py
+++ ontologytimemachine/proxy.py
@@ -1,20 +1,50 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading
+import requests
 
 PORT = 8080
 
 class ProxyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        # static usecase to check the Proxy is working
         if 'google.com' in self.path:
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'This is a static response for google.com')
         else:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            self.wfile.write(b'Resource not found')
+            print(self.path)
+            ontology = self.path[1:]
+            print(ontology)
+            try:
+                response = requests.get(ontology)
+                content_type = response.headers.get('Content-Type', '')
+
+                if response.status_code == 200 and content_type in ['text/turtle']:
+                    self.send_response(200)
+                    self.send_header('Content-type', content_type)
+                    self.end_headers()
+                    self.wfile.write(response.content)
+                else:
+                    self.fetch_from_dbpedia_archivo_api(ontology)
+
+            except requests.exceptions.RequestException:
+                self.fetch_from_dbpedia_archivo_api(ontology)
+
+    def fetch_from_dbpedia_archivo_api(self, ontology):
+        dbpedia_url = f'https://archivo.dbpedia.org/download?o={ontology}&f=ttl'
+        print(dbpedia_url)
+        try:
+            response = requests.get(dbpedia_url)
+            if response.status_code == 200:
+                self.send_response(200)
+                self.send_header('Content-type', 'text/turtle')
+                self.end_headers()
+                self.wfile.write(response.content)
+            else:
+                self.send_error(404, 'Resource not found')
+        except requests.exceptions.RequestException:
+            self.send_error(404, 'Resource not found')
 
 def run_proxy(server_class=HTTPServer, handler_class=ProxyHandler, port=PORT):
     server_address = ('', port)
