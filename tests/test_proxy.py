baeaefbebb1cd344c216daaf980baf1b0e90c0a5
Jenifer Tabita Ciuciu-Kiss <jenifer.tabita.ciuciu.kiss@gmail.com>
2024-06-09 21:56:05 +0200

    Rewriting the proxy using the proxy.py package

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,30 +1,58 @@
 import pytest
 import requests
-from ontologytimemachine.proxy import start_proxy, PORT
+import time
+import proxy
+import subprocess
+import sys
+from ontologytimemachine.custom_proxy import OntologyTimeMachinePlugin
 
-PROXY_URL = f'http://localhost:{PORT}'
+PORT = 8899
+IP = '0.0.0.0'
+PROXY = f'{IP}:{PORT}'
+HTTP_PROXY = f'http://{PROXY}'
+HTTPS_PROXY = f'https://{PROXY}'
 
 @pytest.fixture(scope="module", autouse=True)
 def start_proxy_server():
-    start_proxy()
-    import time
-    time.sleep(1)
+    # Start the proxy server in a subprocess
+    process = subprocess.Popen(
+        [
+            'python3', '-m', 'proxy', 
+            '--hostname', IP, 
+            '--port', str(PORT), 
+            '--plugins', 'ontologytimemachine.custom_proxy.OntologyTimeMachinePlugin'
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    
+    # Wait a bit to ensure the server starts
+    time.sleep(5)
+    
+    yield
+    
+    # Terminate the proxy server after tests
+    process.terminate()
+    process.wait()
 
 def test_google_com():
-    response = requests.get(PROXY_URL + '/google.com')
+    response = requests.get('http://google.com', proxies={'http': HTTP_PROXY, 'https': HTTPS_PROXY})
     assert response.status_code == 200
     assert response.text == 'This is a static response for google.com'
 
 def test_linked_web_apis():
-    response = requests.get(PROXY_URL + '/http://linked-web-apis.fit.cvut.cz/ns/core')
+    iri = 'http://linked-web-apis.fit.cvut.cz/ns/core'
+    response = requests.get(iri, proxies={'http': HTTP_PROXY, 'https': HTTPS_PROXY})
     assert response.status_code == 200
     assert response.headers['Content-Type'] == 'text/turtle'
+    assert iri in response.content.decode('utf-8')
 
 def test_ontologi_es():
-    response = requests.get(PROXY_URL + '/http%3A//ontologi.es/days%23')
+    iri = 'http://ontologi.es/days#'
+    response = requests.get(iri, proxies={'http': HTTP_PROXY, 'https': HTTPS_PROXY})
     assert response.status_code == 200
     assert response.headers['Content-Type'] == 'text/turtle'
-
+    assert iri in response.content.decode('utf-8')
 
 if __name__ == '__main__':
     pytest.main()
